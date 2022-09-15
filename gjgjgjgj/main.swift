ef23026fe6113534acb51c36d0607ac385abc3a4
Magdiel <magdiel.altynbekov@gmail.com>
2022-09-15 08:35:51 +0600

    my homework

--- gjgjgjgj/main.swift
+++ gjgjgjgj/main.swift
@@ -9,5 +9,70 @@ import Foundation
 
 print("Hello, World!")
 
-let a: Int = 123
-var b: Int = 234
+
+func kvadrat (lock: Int){
+    print("квадрат \(lock) равен \(lock * lock)")
+}
+kvadrat(lock: 11)
+
+func kub (lock: Int){
+    print("куб \(lock) равен \(lock * lock * lock)")
+}
+
+
+let a = 10
+func perimetr (lock: Int){
+    print("периметр прямоугольника с длиной \(a) и шириной \(lock) = \((a + lock) * 2)")
+}
+print(perimetr(lock: 11))
+
+
+let S = 3
+let B = 4
+func SHREK (N: Int){
+    print("площадь прямоугольника = \(S * B)")
+}
+SHREK(N: B)
+
+
+
+var R = 2.0
+var P = 3.14
+func Skruga (D: Double) {
+    print(" площадь круга = \((R * R) * 3.14)")
+}
+Skruga(D: R)
+
+
+
+
+
+//N2
+let K = 40
+var C = 12
+func minu(LOL: Int){
+    print("\(K) - \(LOL) = \(K - LOL)")
+}
+
+minu(LOL: 5)
+minu(LOL: 4)
+func pilus(LMAO: Int){
+    print("\(C) + \(LMAO) = \(C + LMAO)")
+}
+pilus(LMAO: 6)
+pilus(LMAO: 66)
+
+func razdelit(salman: Int){
+    print("\(K) : \(salman) = \(K / salman)")
+}
+razdelit(salman: 20)
+razdelit(salman: 4)
+razdelit(salman: 10)
+
+func umnojit(kar: Int){
+    print("\(C) * \(kar) = \(C * kar)")
+}
+umnojit(kar: 3)
+umnojit(kar: 6)
+umnojit(kar: 4)
+
